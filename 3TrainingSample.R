71ddce8ad82d3b7c8ca18a9ad383947aed0fcc35
r.lazcano.pello <rafalaz01@gmail.com>
2018-04-25 17:01:23 +0200

    improved trainingsample.R

--- 3TrainingSample.R
+++ 3TrainingSample.R
@@ -10,5 +10,20 @@ sample_blogs   <- sample(blogs, 3000)
 sample_news    <- sample(news, 3000)
 sample_twitter <- sample(twitter, 3000)
 
+#Join all 3 samples in one
+sample<-rbind(sample_blogs,sample_news,sample_twitter)
+
+#remove obsolete variables
+rm(blogs,news,twitter)
+
+#The package tm is really powerful:  VectorSource creates a text source from the sample and VCorpus (Volatile Corpus because it will dissapear) 
+# creates a corpus where each element of the previous matrix is considered a document.
+mycorpus<-VCorpus(VectorSource(sample))
+mycorpus <- tm_map(mycorpus, content_transformer(tolower))
+mycorpus <- tm_map(mycorpus, removePunctuation) # remove punctuation
+mycorpus <- tm_map(mycorpus, removeNumbers)
+mycorpus <- tm_map(mycorpus, stripWhitespace)
+changetospace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+mycorpus <- tm_map(mycorpus, changetospace, "/|@|\\|")
 # save samples
-save(sample_blogs, sample_news, sample_twitter, file= "sampleData.RData")
+save(sample, file= "sampleData.RData")
